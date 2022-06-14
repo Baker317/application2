5e48da954b1dd7894468f4f493f3bbb0daa04573
Kyran Baker <kbaker@xpanxion.com>
2022-06-14 16:41:43 -0400

    Weeks

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,26 @@ public class Hello
     {
         System.out.println("Hello, World!!!");
 
+        //multiplies the two numbers and prints to screen
+        int number1 = 5;
+        int number2 = 20;
+
+
+        System.out.println(number1 * number2);
+
+
+
+        /* short number3 = 4;
+        byte number4 = 7;
+        float number5 = 5.69F;
+        double number2 = 4.25;
+        boolean isTrue = true;
+        char x = 'x';
+        String greet = "Hello"; */
+
+
     }
 }
+
+
 
